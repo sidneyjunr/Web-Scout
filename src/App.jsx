5b3ccafcdf87e58f9066ca687b4cb3880bd66fed
Sidney <sidneyferreirabaptistajunior3@gmail.com>
2025-03-07 04:00:30 -0300

    Só atualização

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,57 @@
-import React, { useState } from 'react'
-import Logo from './assets/logo.png'
+import React, { useState } from "react";
+import Logo from "./assets/logo.png";
+import { Form } from "./Components/Form";
 
 export const App = () => {
-
   function handleSubmit(e) {
-    e.preventDefault()
-    setSlide(slide + 1)
-    console.log(slide);
-    
-
-
+    e.preventDefault();
+    setSlide(slide + 1);
   }
 
-  const [timeA, setTimeA] = useState('')
-  const [timeB, setTimeB] = useState('')
-  const [slide, setSlide] = useState(0)
+  const [timeA, setTimeA] = useState("");
+  const [timeB, setTimeB] = useState("");
+  const [slide, setSlide] = useState(0);
 
   return (
     <>
-      <div className=' bg-gray-100'>
-        <header className='container mx-auto rounded-b-lg bg-white px-2'>
-          <nav className='flex justify-between items-center mx-4 '>
-            <img src={Logo} className='size-16' alt="logo" />
-            <ul className=''>
-              <li >Contribuir ☕</li>
+      <div className=" bg-gray-100">
+        <header className="container mx-auto rounded-b-lg bg-white px-2">
+          <nav className="flex justify-between items-center mx-4 ">
+            <img src={Logo} className="size-16" alt="logo" />
+            <ul className="">
+              <li>Contribuir ☕</li>
             </ul>
           </nav>
         </header>
-        <main className='container mx-auto h-[calc(100vh-64px)] flex justify-center '>
+        <main className="container mx-auto h-[calc(100vh-64px)] flex justify-center ">
           {slide === 0 ? (
-            <form className='mt-20' onSubmit={handleSubmit}>
-              <label htmlFor='teamA' className='block'>Time A</label>
-              <input
-                id='teamA'
-                className='block mt-2'
-                type="text"
-                placeholder='Digite o nome do time A'
-                value={timeA}
-                onChange={({target})=>setTimeA(target.value.toUpperCase())}
-                required
-              />
-              <button className='mt-4' >Avançar</button>
-            </form>
-
-          ) : null}
-          {slide === 1 ?(
-            <form className='mt-20' onSubmit={handleSubmit}>
-            <label htmlFor='teamB' className='block'>Time B</label>
-            <input
-              id='teamB'
-              className='block mt-2'
-              type="text"
-              placeholder='Digite o nome do time B'
-              value={timeB}
-              onChange={({target})=>setTimeB(target.value.toUpperCase())}
-              required
+            <Form
+              id={"timeA"}
+              value={timeA}
+              label={"Time A"}
+              setTime={setTimeA}
+              placeholder={"Digite o nome do time A"}
+              handleSubmit ={handleSubmit}
             />
-            <button className='mt-4' >Avançar</button>
-          </form>
-          ): null}
-        {timeA && timeB && slide === 2 && <p>{timeA} x {timeB}</p>}
+          ) : null}
+          {slide === 1 ? (
+            <Form
+              id={"timeB"}
+              value={timeB}
+              label={"Time B"}
+              setTime={setTimeB}
+              placeholder={"Digite o nome do time B"}
+              handleSubmit ={handleSubmit}
+
+            />
+          ) : null}
+          {timeA && timeB && slide === 2 && (
+            <p>
+              {timeA} x {timeB}
+            </p>
+          )}
         </main>
       </div>
     </>
-  )
-}
+  );
+};
---
+++ src/Components/Form.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const Form = ({ id, label, value, setTime,handleSubmit, ...props }) => {
+  return (
+    <form className="mt-20 flex flex-col items-center" onSubmit={handleSubmit}>
+      <label htmlFor={id} className="block">
+        {label}
+      </label>
+      <input
+        id={id}
+        className="block mt-2 ring-1 ring-black rounded-full py-2 px-4"
+        type="text"
+        value={value}
+        onChange={({ target }) => setTime(target.value.toUpperCase())}
+        required
+        {...props}
+      />
+      <button className="mt-4 bg-blue-200 px-4 py-2 rounded-full ">Avançar</button>
+    </form>
+  );
+};
